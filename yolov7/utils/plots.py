62140a3be562010bac639b4ba97643a14c60d0e6
Thanh Duy <sologame37@gmail.com>
2023-09-16 23:27:42 +0700

    Add transparent background of bounding box.

--- yolov7/utils/plots.py
+++ yolov7/utils/plots.py
@@ -57,15 +57,22 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
 def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     # Plots one bounding box on image img
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
+    color = [50, 161, 57]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    alpha = 0.4
+
+    overlay = img.copy()
+    cv2.rectangle(overlay, c1, c2, color, -1, cv2.LINE_AA)  # filled
+    img_new = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.rectangle(img_new, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img_new, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+    return img_new
 
 
 def plot_one_box_PIL(box, img, color=None, label=None, line_thickness=None):

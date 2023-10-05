55d9f4970210e21ed542df6b7c52775aaf848bfa
Thanh Duy <sologame37@gmail.com>
2023-10-05 11:34:54 +0700

    Fix some bugs.

--- yolov7/webcam_colab.py
+++ yolov7/webcam_colab.py
@@ -1,9 +1,12 @@
 # Webcam
-import base64
 from IPython.display import display, Javascript, Image
 import PIL
 import io
 import numpy as np
+
+from google.colab.output import eval_js
+from google.colab.patches import cv2_imshow
+from base64 import b64decode, b64encode
 
 import argparse
 import time
@@ -47,15 +50,10 @@ opt = {
 }
 
 class ColabWebCam:
-    def __init__(opt):
-        self.source = opt['source']
-        self.weights = opt.['weights']
-        self.view_img = opt['view_img']
-        self.save_txt = ['opt.save_txt']
-        self.imgsz = opt['img_size']
-        self.trace = not opt['no_trace']
+    def __init__(self):
+        print('Running...')
 
-    def video_stream():
+    def video_stream(self):
         js = Javascript('''
             var video;
             var div = null;
@@ -186,12 +184,12 @@ class ColabWebCam:
 
         display(js)
   
-    def video_frame(label, bbox):
+    def video_frame(self, label, bbox):
         data = eval_js('stream_frame("{}", "{}")'.format(label, bbox))
         return data
 
     # function to convert the JavaScript object into an OpenCV image
-    def js_to_image(js_reply):
+    def js_to_image(self, js_reply):
         """
         Params:
                 js_reply: JavaScript object containing image from webcam
@@ -208,7 +206,7 @@ class ColabWebCam:
         return img
 
     # function to convert OpenCV Rectangle bounding box image into base64 byte string to be overlayed on video stream
-    def bbox_to_bytes(bbox_array):
+    def bbox_to_bytes(self, bbox_array):
         """
             Params:
                     bbox_array: Numpy array (pixels) containing rectangle to overlay on video stream.
@@ -225,7 +223,7 @@ class ColabWebCam:
 
         return bbox_bytes
     
-    def letterbox(img, new_shape=(640, 640), color=(114, 144, 144), auto=True, scaleFill=False, scaleup=True, stride=32):
+    def letterbox(self, img, new_shape=(640, 640), color=(114, 144, 144), auto=True, scaleFill=False, scaleup=True, stride=32):
         shape = img.shape[:2]
         if isinstance(new_shape, int):
             new_shape = (new_shape, new_shape)
@@ -255,8 +253,8 @@ class ColabWebCam:
         img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
         return img, ratio, (dw, dh)
     
-    def start_webcam():
-        video_stream()
+    def start_webcam(self):
+        self.video_stream()
         label_html = 'Capturing...'
         bbox = ''
 
@@ -282,70 +280,69 @@ class ColabWebCam:
             # Run inference
             if device.type != 'cpu':
        	        model(torch.zeros(1, 3, imgsz[0], imgsz[1]).to(device).type_as(next(model.parameters())))  # run once
-    
+            
             while True:
-                js_reply = video_frame(label_html, bbox)
-                if not js_reply:
-                    break
+              js_reply = self.video_frame(label_html, bbox)
+              if not js_reply:
+                break
         
-                img0 = js_to_image(js_reply['img'])
-                bbox_array = np.zeros([480, 640, 4], dtype=np.uint8)
-                img = letterbox(img0, imgsz, stride=stride)[0]
-                img = img[:, :, ::-1].transpose(2, 0, 1)
-                img = np.ascontiguousarray(img)
-                img = torch.from_numpy(img).to(device)
-                img = img.half() if half else img.float()  # uint8 to fp16/32
-                img /= 255.0  # 0 - 255 to 0.0 - 1.0
-                if img.ndimension() == 3:
-                    img = img.unsqueeze(0)
+              img0 = self.js_to_image(js_reply['img'])
+              bbox_array = np.zeros([480, 640, 4], dtype=np.uint8)
+              img = self.letterbox(img0, imgsz, stride=stride)[0]
+              img = img[:, :, ::-1].transpose(2, 0, 1)
+              img = np.ascontiguousarray(img)
+              img = torch.from_numpy(img).to(device)
+              img = img.half() if half else img.float()  # uint8 to fp16/32
+              img /= 255.0  # 0 - 255 to 0.0 - 1.0
+              if img.ndimension() == 3:
+                img = img.unsqueeze(0)
 		    
 	            # Inference
-                t1 = time_synchronized()
-                with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+              t1 = time_synchronized()
+              with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
                 pred = model(img, augment=False)[0]
-                t2 = time_synchronized()
+              t2 = time_synchronized()
 
 	            # Apply NMS
-                pred = non_max_suppression(pred, opt['conf_thres'], opt['iou_thres'], classes=opt['classes'], agnostic=opt['agnostic_nms'])
-                t3 = time_synchronized()
+              pred = non_max_suppression(pred, opt['conf_thres'], opt['iou_thres'], classes=opt['classes'], agnostic=opt['agnostic_nms'])
+              t3 = time_synchronized()
 		
 	            # Process detections
-                for i, det in enumerate(pred):  # detections per image
-                    gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-		    
-                    if len(det):
-                        # Rescale boxes from img_size to im0 size
-                        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
-          
-                        if USE_SORT_TRACKER:
-                            detections_ = []
-            
-                            for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
-                                detections_.append([x1, y1, x2, y2, conf])
-              
-                            track_ids = sort_tracker.update(np.asarray(detections_))
-		                    # Draw boxes for visualization
-                            for track in track_ids:
-                                x1, y1, x2, y2, LP_id = track
-                                license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
-                                assign_number_license_plate(LP_id, license_plate_text, img0[int(y1):int(y2), int(x1):int(x2), :])
-                                label = f'ID: {int(LP_id)} {license_plate[LP_id]["text"]}'
-                                bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
-                        else:
-                            # Write results
-                            for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
-                                license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
-                                is_correct_format = check_format_number_license_plate(license_plate_text)
-                                label = license_plate_text if is_correct_format else ''
-                                bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
+              for i, det in enumerate(pred):  # detections per image
+                gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                if len(det):
+                  # Rescale boxes from img_size to im0 size
+                  det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+                  
+                  if USE_SORT_TRACKER:
+                    detections_ = []
+                    
+                    for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
+                      detections_.append([x1, y1, x2, y2, conf])
+                    
+                    track_ids = sort_tracker.update(np.asarray(detections_))
+		                # Draw boxes for visualization
+                    for track in track_ids:
+                      x1, y1, x2, y2, LP_id = track
+                      license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
+                      assign_number_license_plate(LP_id, license_plate_text, img0[int(y1):int(y2), int(x1):int(x2), :])
+                      label = f'ID: {int(LP_id)} {license_plate[LP_id]["text"]}'
+                      bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
+                  else:
+                      # Write results
+                      for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
+                        license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
+                        is_correct_format = check_format_number_license_plate(license_plate_text)
+                        label = license_plate_text if is_correct_format else ''
+                        bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
       
-                    elif USE_SORT_TRACKER:
+                elif USE_SORT_TRACKER:
 		                track_ids = sort_tracker.update()
 		                # SORT should be updated even with no detections
-            
-                bbox_array[:, :, 3] = (bbox_array.max(axis = 2) > 0).astype(int) * 255
-                bbox_bytes = bbox_to_bytes(bbox_array)
-                bbox = bbox_bytes
+              
+              bbox_array[:, :, 3] = (bbox_array.max(axis = 2) > 0).astype(int) * 255
+              bbox_bytes = self.bbox_to_bytes(bbox_array)
+              bbox = bbox_bytes
 
 cam = ColabWebCam()
 cam.start_webcam()

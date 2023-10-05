df5f797ad2d0b8f732279a4db9ecf5d8b40e7eb0
Thanh Duy <sologame37@gmail.com>
2023-10-05 17:02:50 +0700

    Update webcam_colab.py

--- yolov7/webcam_colab.py
+++ yolov7/webcam_colab.py
@@ -20,41 +20,58 @@ from numpy import random
 
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.general import (
+    check_img_size,
+    check_requirements,
+    check_imshow,
+    non_max_suppression,
+    apply_classifier,
+    scale_coords,
+    xyxy2xywh,
+    strip_optimizer,
+    set_logging,
+    increment_path,
+)
 from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from utils.torch_utils import (
+    select_device,
+    load_classifier,
+    time_synchronized,
+    TracedModel,
+)
 
 # LP Check
 from check_LP import *
 
 opt = {
-    'weight': '/content/drive/MyDrive/Yolov7_Train/train_models/LP_detect_yolov7_500img.pt',
-	'source': '0',
-	'img_size': 640,
-	'conf_thres': 0.25,
-	'iou_thres': 0.45,
-	'device': '',
-	'view_img': False,
-	'save_txt': False,
-	'save_conf': False,
-	'nosave': False,
-	'classes': None,
-	'agnostic_nms': False,
-	'augment': False,
-	'update': False,
-	'project': 'runs/detect',
-	'name': 'exp',
-	'exist_ok': False,
-	'no_trace': True,
+    "weight": "/content/drive/MyDrive/Yolov7_Train/train_models/LP_detect_yolov7_500img.pt",
+    "source": "0",
+    "img_size": 640,
+    "conf_thres": 0.25,
+    "iou_thres": 0.45,
+    "device": "",
+    "view_img": False,
+    "save_txt": False,
+    "save_conf": False,
+    "nosave": False,
+    "classes": None,
+    "agnostic_nms": False,
+    "augment": False,
+    "update": False,
+    "project": "runs/detect",
+    "name": "exp",
+    "exist_ok": False,
+    "no_trace": True,
 }
+
 
 class ColabWebCam:
     def __init__(self):
-        print('Running...')
+        print("Running...")
 
     def video_stream(self):
-        js = Javascript('''
+        js = Javascript(
+            """
             var video;
             var div = null;
             var stream;
@@ -181,10 +198,11 @@ class ColabWebCam:
                     'capture': Date.now() - preCapture,
                     'img': result};
             }
-        ''')
+        """
+        )
 
         display(js)
-  
+
     def video_frame(self, label, bbox):
         data = eval_js('stream_frame("{}", "{}")'.format(label, bbox))
         return data
@@ -198,7 +216,7 @@ class ColabWebCam:
                 img: OpenCV BGR image
         """
         # decode base64 image
-        image_bytes = b64decode(js_reply.split(',')[1])
+        image_bytes = b64decode(js_reply.split(",")[1])
         # convert bytes to numpy array
         jpg_as_np = np.frombuffer(image_bytes, dtype=np.uint8)
         # decode numpy array into OpenCV BGR image
@@ -209,30 +227,32 @@ class ColabWebCam:
     # function to convert OpenCV Rectangle bounding box image into base64 byte string to be overlayed on video stream
     def bbox_to_bytes(self, bbox_array):
         """
-            Params:
-                    bbox_array: Numpy array (pixels) containing rectangle to overlay on video stream.
-            Returns:
-                    bytes: Base64 image byte string
+        Params:
+                bbox_array: Numpy array (pixels) containing rectangle to overlay on video stream.
+        Returns:
+                bytes: Base64 image byte string
         """
         # convert array into PIL image
-        bbox_PIL = PIL.Image.fromarray(bbox_array, 'RGBA')
+        bbox_PIL = PIL.Image.fromarray(bbox_array, "RGBA")
         iobuf = io.BytesIO()
         # format bbox into png for return
-        bbox_PIL.save(iobuf, format='png')
+        bbox_PIL.save(iobuf, format="png")
         # format return string
-        bbox_bytes = 'data:image/png;base64,{}'.format((str(b64encode(iobuf.getvalue()), 'utf-8')))
+        bbox_bytes = "data:image/png;base64,{}".format(
+            (str(b64encode(iobuf.getvalue()), "utf-8"))
+        )
 
         return bbox_bytes
-    
-    def letterbox(self, img, new_shape=(640, 640), color=(114, 144, 144), auto=True, scaleFill=False, scaleup=True, stride=32):
+
+    def letterbox(self, img, new_shape = (640, 640), color = (114, 144, 144), auto = True, scaleFill = False, scaleup = True, stride = 32):
         shape = img.shape[:2]
         if isinstance(new_shape, int):
             new_shape = (new_shape, new_shape)
-  
+
         r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
         if not scaleup:
             r = min(r, 1.0)
-  
+
         ratio = r, r
         new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
         dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
@@ -248,101 +268,114 @@ class ColabWebCam:
 
         if shape[::-1] != new_unpad:
             img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-  
+
         top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
         left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-        img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+        img = cv2.copyMakeBorder(
+            img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+        )
         return img, ratio, (dw, dh)
-    
+
     def start_webcam(self):
         self.video_stream()
-        label_html = 'Capturing...'
-        bbox = ''
+        label_html = "Capturing..."
+        bbox = ""
 
         with torch.no_grad():
-            weights, imgsz = opt['weight'], (480, 640)
-	        # Initialize
+            weights, imgsz = opt["weight"], (480, 640)
+            # Initialize
             set_logging()
-            device = select_device('')
-            half = device.type != 'cpu'  # half precision only supported on CUDA
+            device = select_device("")
+            half = device.type != "cpu"  # half precision only supported on CUDA
 
             # Load model
             model = attempt_load(weights, map_location=device)  # load FP32 model
             stride = int(model.stride.max())  # model stride
             # imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    	
+
             if half:
                 model.half()  # to FP16
-        	
-	        # Get names and colors
-            names = model.module.names if hasattr(model, 'module') else model.names
+
+            # Get names and colors
+            names = model.module.names if hasattr(model, "module") else model.names
             colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
             # Run inference
-            if device.type != 'cpu':
-       	        model(torch.zeros(1, 3, imgsz[0], imgsz[1]).to(device).type_as(next(model.parameters())))  # run once
-            
-            while True:
-              js_reply = self.video_frame(label_html, bbox)
-              if not js_reply:
-                break
-        
-              img0 = self.js_to_image(js_reply['img'])
-              bbox_array = np.zeros([480, 640, 4], dtype=np.uint8)
-              img = self.letterbox(img0, imgsz, stride=stride)[0]
-              img = img[:, :, ::-1].transpose(2, 0, 1)
-              img = np.ascontiguousarray(img)
-              img = torch.from_numpy(img).to(device)
-              img = img.half() if half else img.float()  # uint8 to fp16/32
-              img /= 255.0  # 0 - 255 to 0.0 - 1.0
-              if img.ndimension() == 3:
-                img = img.unsqueeze(0)
-		    
-	            # Inference
-              t1 = time_synchronized()
-              with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-                pred = model(img, augment=False)[0]
-              t2 = time_synchronized()
+            if device.type != "cpu":
+                model(
+                    torch.zeros(1, 3, imgsz[0], imgsz[1])
+                    .to(device)
+                    .type_as(next(model.parameters()))
+                )  # run once
 
-	            # Apply NMS
-              pred = non_max_suppression(pred, opt['conf_thres'], opt['iou_thres'], classes=opt['classes'], agnostic=opt['agnostic_nms'])
-              t3 = time_synchronized()
-		
-	            # Process detections
-              for i, det in enumerate(pred):  # detections per image
-                gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-                if len(det):
-                  # Rescale boxes from img_size to im0 size
-                  det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
-                  
-                  if USE_SORT_TRACKER:
-                    detections_ = []
-                    
-                    for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
-                      detections_.append([x1, y1, x2, y2, conf])
-                    
-                    track_ids = sort_tracker.update(np.asarray(detections_))
-		            # Draw boxes for visualization
-                    for track in track_ids:
-                      x1, y1, x2, y2, LP_id = track
-                      license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
-                      assign_number_license_plate(LP_id, license_plate_text, img0[int(y1):int(y2), int(x1):int(x2), :])
-                      label = f'ID: {int(LP_id)} {license_plate[LP_id]["text"]}'
-                      bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
-                  else:
-                      # Write results
-                      for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
-                        license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
-                        is_correct_format = check_format_number_license_plate(license_plate_text)
-                        label = license_plate_text if is_correct_format else ''
-                        bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
-      
-                elif USE_SORT_TRACKER:
-		                track_ids = sort_tracker.update() # SORT should be updated even with no detections
-              
-              bbox_array[:, :, 3] = (bbox_array.max(axis = 2) > 0).astype(int) * 255
-              bbox_bytes = self.bbox_to_bytes(bbox_array)
-              bbox = bbox_bytes
+            while True:
+                js_reply = self.video_frame(label_html, bbox)
+                if not js_reply:
+                    break
+
+                img0 = self.js_to_image(js_reply["img"])
+                bbox_array = np.zeros([480, 640, 4], dtype=np.uint8)
+                img = self.letterbox(img0, imgsz, stride=stride)[0]
+                img = img[:, :, ::-1].transpose(2, 0, 1)
+                img = np.ascontiguousarray(img)
+                img = torch.from_numpy(img).to(device)
+                img = img.half() if half else img.float()  # uint8 to fp16/32
+                img /= 255.0  # 0 - 255 to 0.0 - 1.0
+                if img.ndimension() == 3:
+                    img = img.unsqueeze(0)
+
+                # Inference
+                t1 = time_synchronized()
+                with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
+                    pred = model(img, augment=False)[0]
+                t2 = time_synchronized()
+
+                # Apply NMS
+                pred = non_max_suppression(
+                    pred,
+                    opt["conf_thres"],
+                    opt["iou_thres"],
+                    classes=opt["classes"],
+                    agnostic=opt["agnostic_nms"],
+                )
+                t3 = time_synchronized()
+
+                # Process detections
+                for i, det in enumerate(pred):  # detections per image
+                    gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                    if len(det):
+                        # Rescale boxes from img_size to im0 size
+                        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+                        if USE_SORT_TRACKER:
+                            detections_ = []
+
+                            for x1, y1, x2, y2, conf, detclass in (det.cpu().detach().numpy()):
+                                detections_.append([x1, y1, x2, y2, conf])
+
+                            track_ids = sort_tracker.update(np.asarray(detections_))
+                            # Draw boxes for visualization
+                            for track in track_ids:
+                                x1, y1, x2, y2, LP_id = track
+                                license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
+                                assign_number_license_plate(LP_id,license_plate_text,img0[int(y1) : int(y2), int(x1) : int(x2), :],)
+                                label = f'ID: {int(LP_id)} {license_plate[LP_id]["text"]}'
+                                bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
+                        else:
+                            # Write results
+                            for x1, y1, x2, y2, conf, detclass in (det.cpu().detach().numpy()):
+                                license_plate_text = read_license_plate(img0, x1, y1, x2, y2)
+                                is_correct_format = check_format_number_license_plate(license_plate_text)
+                                label = license_plate_text if is_correct_format else ""
+                                bbox_array = plot_one_box([x1, y1, x2, y2], bbox_array, label=label, line_thickness=4)
+								
+                    elif USE_SORT_TRACKER:
+                        track_ids = sort_tracker.update()  # SORT should be updated even with no detections
+
+                bbox_array[:, :, 3] = (bbox_array.max(axis=2) > 0).astype(int) * 255
+                bbox_bytes = self.bbox_to_bytes(bbox_array)
+                bbox = bbox_bytes
+
 
 cam = ColabWebCam()
 cam.start_webcam()
